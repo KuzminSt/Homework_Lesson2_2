4a68951deb6299ab9d7f193a18855a5f437845ae
kuzminst@gmail.com <kuzminst@gmail.com>
2020-06-15 10:32:20 +0300

    коммит

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Homework_Lesson2_2
+namespace HomeWork_Lesson2_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+              
+            Console.WriteLine("Введите название продукта:");             //Запрашиваем Название
+            string prod1 = Console.ReadLine();            
+            prod1 = prod1.Trim().ToUpper();
+            Console.WriteLine("Введите единицы измерения:" );               //Запрашиваем Ед измерения
+            string measure = Console.ReadLine();
+            measure = measure.Trim().ToLower();
+            Console.WriteLine("Введите количество продукта:");           //Запрашиваем количество 
+            string quantity = Console.ReadLine();
+            quantity = quantity.Trim();
+            quantity = quantity.Replace(".", ",");                     //Приводим количество  к дробному числу
+            double quantityDouble = Convert.ToDouble(quantity);
+            Console.WriteLine("Введите цену продукта за 1 ед:");          //Запрашиваем цену
+            string price = Console.ReadLine();
+            price = price.Trim();
+            price = price.Replace(".", ",");                         //Приводим цену к дробному числу
+            double priceDouble = Convert.ToDouble(price);
+
+            Console.Clear();
+
+            Console.WriteLine($"Наименование продукта: {prod1}\nЕдиницы измерения: {measure}");
+            Console.WriteLine($"Цена за единицу: {priceDouble}\nКоличество: {quantityDouble}");
+            Console.ReadLine();
+
+
         }
     }
 }
